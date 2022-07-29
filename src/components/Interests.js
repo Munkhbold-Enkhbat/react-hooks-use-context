fd6b014e36e15ee0353c6686c37ca91d0f7ba35b
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-07-29 12:59:05 -0700

    Completed Assignment

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/theme";
+import { UserContext } from "../context/user"
 
-function Interests({ interests, theme }) {
+function Interests() {
+
+  const { user } = useContext(UserContext)
+  const themeData = useContext(ThemeContext)
+  
   return (
     <div>
       <h4>Interests</h4>
-      <ul className={theme}>
-        {interests.map((interest) => (
+      <ul className={themeData.theme}>
+        {user.interests.map((interest) => (
           <li key={interest}>{interest}</li>
         ))}
       </ul>
---
+++ src/context/user.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const UserContext = React.createContext()
+
+function UserProvider({ children }) {
+  const [user, setUser] = useState({
+    name: "Duane",
+    interests: ["Coding", "Biking", "Words ending in 'ing'"]
+  })
+
+  return <UserContext.Provider value={{ user, setUser }}>{ children }</UserContext.Provider>
+}
+
+export { UserContext, UserProvider }
